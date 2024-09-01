0adb9dca64866b62111c4c57f97df890796ca755
Jihyeok <qowlgur121@gmail.com>
2024-09-01 20:36:41 +0900

    서블릿으로 GET 쿼리 파라미터 조회

--- src/main/java/com/hello/servlet/ServletApplication.java
+++ src/main/java/com/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package com.hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //현재 패키지와 그 하위 패키지에서 서블릿을 찾아서 등록한다.
 @SpringBootApplication
 public class ServletApplication {
 
